7459228a27da539bb40a3609d3f93de610476864
pmercado23 <pmercado@ucsc.edu>
2018-04-30 07:00:35 -0700

    Space Shooter Demo

---
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public int scoreValue;
+	private GameConroller gameController;
+
+	void Start(){
+	
+		GameObject gameControllerObject = GameObject.FindGameObjectWithTag ("GameController");
+		if (gameControllerObject != null) {
+
+			gameController = gameControllerObject.GetComponent <GameConroller> ();
+		}
+		if (gameController == null) {
+		
+			Debug.Log ("Error: Cannot Find 'GameController' script.");
+		}
+	
+	}
+
+
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("Boundary") || other.CompareTag ("Enemy")) {
+			return;
+		}
+
+		if (explosion != null) {
+			Instantiate (explosion, transform.position, transform.rotation);
+		}
+		if (other.CompareTag ("Player")) {
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver ();
+		}
+
+		gameController.AddScore(scoreValue);
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
