4b076235232aae192782aa3f42712f5e22d2f621
Peter Mercado <peter.mercado255@gmail.com>
2018-09-28 10:54:58 -0400

    finsihg Number wizard

---
+++ udemy/Unity2D/Text101/Text101/Assets/Scripts/State.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+
+    //This is a State, Scripable Object
+    [TextArea(10,14)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory (){
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+
+}
